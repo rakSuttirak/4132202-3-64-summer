336007b95c2f2845f0ed5c08a718fad8189a3646
rakSuttirak <93894147+rakSuttirak@users.noreply.github.com>
2022-04-22 12:00:18 +0700

    Add files via upload

---
+++ lab4/www/php/profile.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Narongsak Ramarueang";
+echo "<br>";
+echo "ID : 630112418007";
+echo "<br>";
+echo "INFORMATION TECHNOLOGY";
+echo "<br>";
+echo "YEAR 2 M.1";
---
+++ lab4/www/query/user_del.php
@@ -0,0 +1,15 @@
+<?php
+require_once('conDB.php');
+
+$name = $_POST['name'];
+$sname = $_POST['sname'];
+$age = $_POST['age'];
+$sex = $_POST['sex'];
+
+$sql = "DELETE from tb_user where name = '$name' ";
+
+try {
+    $mysqli->query($sql);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
